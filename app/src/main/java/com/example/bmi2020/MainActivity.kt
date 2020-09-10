5197772a00159bc5acb833d7000bbed0050fcf34
Kai-Wei Sheu <a5255832@g.pccu.edu.tw>
2020-09-10 17:13:34 +0800

    linearLayout

--- app/src/main/java/com/example/bmi2020/MainActivity.kt
+++ app/src/main/java/com/example/bmi2020/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.bmi2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun bmi (view: View){
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val bmi = weight / (height*height)
+        Log.d("BMI",bmi.toString())
     }
 
 }
